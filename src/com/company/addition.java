dd943bb7e77ddd2fc684777ba58e57ef248abbde
yaminicodes <yamini.mhalas@cognologix.com>
2021-08-18 21:30:21 +0530

    addition code

---
+++ src/com/company/addition.java
@@ -0,0 +1,11 @@
+package com.company;
+
+
+public class addition {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int c = a + b;
+        System.out.println(c);
+    }
+}
